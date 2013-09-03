5f9e6ae4072b822c8aa3d1de50bf7b9f6b8bd897
Timothy Pratley <timothypratley@gmail.com>
2013-09-03 07:38:40 -0700

    reserve direction and use 8 bins

--- src/tablescrape/core.clj
+++ src/tablescrape/core.clj
@@ -19,8 +19,25 @@ counts
 ; do the dates that we have align?
 (= (map #(take m (map first %)) [sap pe spe]))
 
+(defn series
+  "Ignore the first value as it is the current estimate.
+  Take only as many as will match the other series, length m.
+  And select only the values (not the dates)."
+  [s]
+  (reverse (rest (take m (map second s)))))
+(series sap)
+
+(view (histogram (series pe) :nbins 50 :title "Distribution of PE" :x-label "PE"))
+(view (histogram (series spe) :nbins 50 :title "Distribution of Shiller PE" :x-label "SPE"))
+
+(def ss (series sap))
+(def monthly-gains (map / (rest ss) ss))
+(def yearly-gains (map / (drop 12 ss) ss))
+(def five-year-gains (map / (drop 60 ss) ss))
+(mean five-year-gains)
+
 ; plot an overlay chart
-(let [date (map (comp to-long first) sap)
+(let [date (reverse (map (comp to-long first) sap))
       chart (time-series-plot date (series sap)
                               :title "Overlay"
                               :y-label "S&P 500 index"
@@ -37,41 +54,24 @@ counts
   (.mapDatasetToRangeAxis plot 2 1)
   (view chart))
 
-(view (histogram (series pe) :nbins 50 :title "Distribution of PE" :x-label "PE"))
-(view (histogram (series spe) :nbins 50 :title "Distribution of Shiller PE" :x-label "SPE"))
-
-
-(defn series
-  "Ignore the first value as it is the current estimate.
-  Take only as many as will match the other series, length m.
-  And select only the values (not the dates)."
-  [s]
-  (rest (take m (map second s))))
-(series sap)
-
-(def ss (series sap))
-(def monthly-gains (map / ss (rest ss)))
-(def yearly-gains (map / ss (drop 12 ss)))
-(def five-year-gains (map / ss (drop 60 ss)))
-(mean five-year-gains)
-
 (view (histogram monthly-gains :nbins 50 :title "Monthly gains"))
 (view (histogram yearly-gains :nbins 50 :title "Yearly gains"))
 (view (histogram five-year-gains :nbins 50 :title "Five year gains"))
 
-(quantile (series pe))
-(quantile (series spe))
+(def bins [0.125 0.25 0.375 0.5 0.625 0.75 0.875])
+(quantile (series pe) :probs bins)
+(quantile (series spe) :probs bins)
 
 (defn quantize-gains
   "Given prices and PE ratios,
   calculate the return for a given period,
   and gather the returns by PE quantile."
   [prices ratios period]
-  (let [quantiles (drop-last (quantile ratios))
+  (let [quantiles (quantile ratios :probs bins)
         quantize (fn [ratio]
                    (count (take-while #(>= ratio %) quantiles)))
         quantized (map quantize ratios)
-        gains (map / prices (drop period prices))
+        gains (map / (drop period prices) prices)
         table (map vector quantized gains)
         indexed (group-by first table)]
     (for [[k v] (sort indexed)]
@@ -95,10 +95,10 @@ counts
         periods [1 3 6 12 24 36 60 120 240 360]
         all-pe (map qpe periods)
         all-pe-means (map #(map mean %) all-pe)
-        quantiles (range 1 5)
+        quantiles (range 1 (+ 2 (count bins)))
         raw (mapcat #(map (fn [x y] [%1 x y]) quantiles %2) periods all-pe-means)
         ;; n normalizes to an APR - uncomment the below if you want to plot adjusted
-        ; raw (mapcat #(map (fn [x y] [%1 x (apr y %1)]) quantiles %2) periods all-pe-means)
+        raw (mapcat #(map (fn [x y] [%1 x (apr y %1)]) quantiles %2) periods all-pe-means)
         ds (dataset [:period :quantile :mean] raw)
         chart (bar-chart :quantile :mean :group-by :period :data ds
                          :title (str "Performance by " label)
@@ -116,18 +116,19 @@ counts
 (defn plot-distributions
   [prices ratios label bins1 bins2]
   (let [qg (quantize-gains prices ratios 12)
-        chart (histogram (last qg) :nbins bins1 :series-label 1
+        chart (histogram (last qg) :nbins bins1 :series-label "last"
                          :title (str "Relative distributions " label)
                          :x-label "Return"
                          :legend true)
         plot (.getPlot chart)
         renderer (.getRenderer plot)]
-    (add-histogram chart (first qg) :nbins bins2 :series-label 4)
+    (add-histogram chart (first qg) :nbins bins2 :series-label "first")
     (.setPaint renderer (java.awt.Color. 0 0 0 128))
     (.setSeriesOutlinePaint renderer 0 java.awt.Color/green)
     (.setSeriesOutlinePaint renderer 1 java.awt.Color/red)
     (view chart)))
 
-(plot-distributions (series sap) (series pe) "PE" 50 25)
-(plot-distributions (series sap) (series spe) "Shiller PE" 25 50)
+(plot-distributions (series sap) (series pe) "PE" 50 50)
+(plot-distributions (series sap) (series spe) "Shiller PE" 30 50)
+
 
